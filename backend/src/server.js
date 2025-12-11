f47eaf00b1e2d1da720fe445be775f4cdcc0bea3
आस्का घिमिरे <meaaska17@gmail.com>
2025-12-11 14:58:02 +0545

    Backend and frontend setup

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Connect DB
+connectDB();
+
+// Test API
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
